164145f259c6c2b3e481edec7600cdc0c2e75a36
omereryilmaz <omer-eryilmaz@hotmail.com>
2021-08-26 14:43:33 +0300

    Create token command added

--- WebAPI/Application/UserOperations/Commands/CreateToken/CreateTokenCommand.cs
+++ WebAPI/Application/UserOperations/Commands/CreateToken/CreateTokenCommand.cs
@@ -4,6 +4,8 @@ using AutoMapper;
 using Microsoft.Extensions.Configuration;
 using WebAPI.DBOperations;
 using WebAPI.Entities;
+using WebAPI.TokenOperations;
+using WebAPI.TokenOperations.Models;
 
 namespace WebAPI.Application.UserOperations.Commands.CreateToken
 {
@@ -20,24 +22,31 @@ namespace WebAPI.Application.UserOperations.Commands.CreateToken
       _configuration = configuration;
     }
 
-    public void Handle()
+    public Token Handle()
     {
-      var user = _context.Users.FirstOrDefault(x => x.Email == Model.Email && x.Password == Model.Password);
-      if(user is not null)
+      var user = _context.Users.FirstOrDefault(x => 
+                                    x.Email == Model.Email && 
+                                    x.Password == Model.Password);
+      if (user is not null)
       {
-        // Create token
-        
+        TokenHandler handler = new TokenHandler(_configuration);
+        Token token = handler.CreateAccessToken(user);
+        user.RefreshToken = token.RefreshToken;
+        user.RefreshTokenExpireDate = token.Expiration.AddMinutes(5);
+        _context.SaveChanges();
+
+        return token;
       }
       else
       {
         throw new InvalidOperationException("Kullanici adi veya sifre hatali!");
       }
     }
+  }
 
-    public class CreateTokenModel
-    {
-      public string Email { get; set; }
-      public string Password { get; set; }
-    }
+  public class CreateTokenModel
+  {
+    public string Email { get; set; }
+    public string Password { get; set; }
   }
 }
